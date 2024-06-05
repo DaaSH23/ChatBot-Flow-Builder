991e97735711c333887d1ad7c52e20a270674410
Abhishek Oraon <abhishekoraon082@gmail.com>
2024-06-05 20:57:12 +0530

    resolved the issue related to drag in canvas

--- src/components/SettingsPanel.js
+++ src/components/SettingsPanel.js
@@ -2,9 +2,14 @@ import React, { useContext } from 'react';
 import { NodeContext } from '../context/FlowContext';
 
 const SettingsPanel = () => {
-  const { selectedNode, handleNodeChange } = useContext(NodeContext);
+  const { selectedNode, handleNodeChange, setPanelChange, showPanel } = useContext(NodeContext);
 
-  if (!selectedNode) return null;
+  console.log(showPanel);
+  if (!selectedNode || !showPanel) return null;
+
+  const handlePanelChange = (event) => {
+    setPanelChange(false);
+  };
 
   const handleInputChange = (e) => {
     const updatedNode = {
@@ -20,13 +25,24 @@ const SettingsPanel = () => {
 
   return (
     <aside>
+      <div className='settingHeader'>
+        <p className='backButton' onClick={handlePanelChange}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+          </svg>
+        </p>
+        <p className='msgText1'>
+          Message
+        </p>
+      </div>
       <div className="settings-panel">
-        <label>Text:</label>
-        <input
-          type="text"
+        <label>Text</label>
+        <textarea
+        className='SettingtextArea'
+        rows="4" cols="30"
           value={selectedNode.data.label}
           onChange={handleInputChange}
-        />
+        ></textarea>
       </div>
     </aside>
   );
